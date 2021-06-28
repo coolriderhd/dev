2e94092c12f8b5ccf2cd3fc334f50d2d7e5fd4e2
coolriderhd <coolrider@vivaldi.net>
2021-06-28 14:03:07 +0200

    test de gestion de branch

---
+++ powershell/tests.ps1
@@ -0,0 +1,23 @@
+﻿function test {
+
+    write-host $param1 "=>" $param2
+
+}
+
+function helping {
+
+    write-host "
+                on entre bien dans la
+                boucle de l'aide
+                "
+}
+
+$param1 = $args[0]
+$param2 = $args[1]
+
+switch ($param1,$param2)
+{
+   {@("Disabled", "Enabled") -notcontains $param1} {helping;break}
+   {@("Disabled", "Enabled") -notcontains $param2} {helping;break}
+   default {test;break}
+}
